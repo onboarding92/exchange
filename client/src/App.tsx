73d2d63f25fea9d406c0d89133ef0787d0b17319
Onboardin92 <benziluca92@gmail.com>
2025-11-25 16:06:43 +0000

    fixed some bugs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,66 +1,76 @@
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
-
-import LoginHistoryPage from "@/pages/LoginHistory";
 import { Route, Switch } from "wouter";
+
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
+
+// UI / layout
+import { Toaster } from "./components/ui/sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
+
+// Pagine utente
 import Home from "./pages/Home";
 import Wallet from "./pages/Wallet";
 import Deposit from "./pages/Deposit";
+import DepositGateway from "./pages/DepositGateway";
 import Withdraw from "./pages/Withdraw";
+import InternalTransfer from "./pages/InternalTransfer";
 import Trading from "./pages/Trading";
+import Staking from "./pages/Staking";
+import Promo from "./pages/Promo";
+import Transactions from "./pages/Transactions";
+import Support from "./pages/Support";
 import Profile from "./pages/Profile";
 import Security from "./pages/Security";
 import LoginHistory from "./pages/LoginHistory";
 import Kyc from "./pages/Kyc";
 import Prices from "./pages/Prices";
-import Staking from "./pages/Staking";
-import Promo from "./pages/Promo";
-import DepositGateway from "./pages/DepositGateway";
-import InternalTransfer from "./pages/InternalTransfer";
-import Transactions from "./pages/Transactions";
-import Support from "./pages/Support";
+
+// Pagine admin
 import AdminLogin from "./pages/AdminLogin";
-import AdminDashboard from "./pages/AdminDashboard";
+import Admin from "./pages/Admin";
 import AdminKyc from "./pages/AdminKyc";
 import AdminLogs from "./pages/AdminLogs";
 import AdminPayments from "./pages/AdminPayments";
 import AdminPromos from "./pages/AdminPromos";
 import AdminSupport from "./pages/AdminSupport";
-import SecurityCenter from "@/pages/SecurityCenter";
+
+// 404
+import NotFound from "./pages/NotFound";
 
 function Router() {
   return (
     <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/wallet"} component={Wallet} />
-      <Route path={"/deposit"} component={Deposit} />
-      <Route path={"/deposit/gateway"} component={DepositGateway} />
-      <Route path={"/withdraw"} component={Withdraw} />
-      <Route path={"/transfer"} component={InternalTransfer} />
-      <Route path={"/trading"} component={Trading} />
-      <Route path={"/staking"} component={Staking} />
-      <Route path={"/promo"} component={Promo} />
-      <Route path={"/transactions"} component={Transactions} />
-      <Route path={"/support"} component={Support} />
-      <Route path={"/profile"} component={Profile} />
-      <Route path={"/security"} component={Security} />
-      <Route path={"/login-history"} component={LoginHistory} />
-      <Route path={"/kyc"} component={Kyc} />
-      <Route path={"/prices"} component={Prices} />
-      <Route path={"/admin-login"} component={AdminLogin} />
-      <Route path={"/admin"} component={AdminDashboard} />
-      <Route path={"/admin/kyc"} component={AdminKyc} />
-      <Route path={"/admin/logs"} component={AdminLogs} />
-      <Route path={"/admin/payments"} component={AdminPayments} />
-      <Route path={"/admin/promos"} component={AdminPromos} />
-      <Route path={"/admin/support"} component={AdminSupport} />
-      <Route path={"/404"} component={NotFound} />
+      {/* User routes */}
+      <Route path="/" component={Home} />
+      <Route path="/wallet" component={Wallet} />
+      <Route path="/deposit" component={Deposit} />
+      <Route path="/deposit/gateway" component={DepositGateway} />
+      <Route path="/withdraw" component={Withdraw} />
+      <Route path="/transfer" component={InternalTransfer} />
+      <Route path="/trading" component={Trading} />
+      <Route path="/staking" component={Staking} />
+      <Route path="/promo" component={Promo} />
+      <Route path="/transactions" component={Transactions} />
+      <Route path="/support" component={Support} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/security" component={Security} />
+      <Route path="/login-history" component={LoginHistory} />
+      <Route path="/kyc" component={Kyc} />
+      <Route path="/prices" component={Prices} />
+
+      {/* Admin routes */}
+      <Route path="/admin-login" component={AdminLogin} />
+      <Route path="/admin" component={Admin} />
+      <Route path="/admin/kyc" component={AdminKyc} />
+      <Route path="/admin/logs" component={AdminLogs} />
+      <Route path="/admin/payments" component={AdminPayments} />
+      <Route path="/admin/promos" component={AdminPromos} />
+      <Route path="/admin/support" component={AdminSupport} />
+
+      {/* 404 */}
+      <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
-      <Route path="/security/logins" component={LoginHistoryPage} />
-        </Switch>
+    </Switch>
   );
 }
 
